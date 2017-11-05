88d59dbf27d6ddc21a49ba66dca86f41a8c4053c
allanpichardo <allan@skmedia.us>
2017-11-05 12:16:33 -0500

    chord timing improved

--- src/components/Synth.jsx
+++ src/components/Synth.jsx
@@ -34,6 +34,7 @@ export default class Synth extends Component {
         this.getStateCopy = this.getStateCopy.bind(this);
         this.playStep = this.playStep.bind(this);
         this.resetAllVoices = this.resetAllVoices.bind(this);
+        this.getVoicesFor = this.getVoicesFor.bind(this);
     }
 
     componentDidMount() {
@@ -68,55 +69,77 @@ export default class Synth extends Component {
         };
     }
 
-    playStep() {
-        for(let i = 0; i < 4; ++i) {
-            let notes = this.state.sequence[i][this.state.step];
-            if(notes) {
-                let voice = this.audioContext.createOscillator();
-                voice.type = this.state.osc1.waveform;
-                voice.connect(this.oscGain1);
-                this.oscGain1.gain.value = this.state.osc1.mix;
+    getVoicesFor(channel) {
+        let notes = this.state.sequence[channel - 1][this.state.step];
+        if(notes) {
+            let voice1 = this.audioContext.createOscillator();
+            voice1.type = this.state.osc1.waveform;
+            voice1.connect(this.oscGain1);
 
-                let newState = this.getStateCopy();
-                newState.voices[0][i] = voice;
-                this.setState(newState);
+            let voice2 = this.audioContext.createOscillator();
+            voice2.type = this.state.osc2.waveform;
+            voice2.connect(this.oscGain2);
 
-                voice.frequency.value = Calculations.shiftToOctave(notes, this.state.osc1.octave);
-                try {
-                    voice.start();
-                }catch (e) {
-                    console.log(e);
-                }
-            }
+            voice1.frequency.value = Calculations.shiftToOctave(notes, this.state.osc1.octave);
+            voice2.frequency.value = Calculations.shiftToOctave(notes, this.state.osc2.octave);
+
+            return [voice1, voice2];
         }
-        for(let i = 0; i < 4; ++i) {
-            let notes = this.state.sequence[i][this.state.step];
-            if(notes) {
-                let voice = this.audioContext.createOscillator();
-                voice.type = this.state.osc2.waveform;
-                voice.connect(this.oscGain2);
-                this.oscGain2.gain.value = this.state.osc2.mix;
 
-                let newState = this.getStateCopy();
-                newState.voices[1][i] = voice;
-                this.setState(newState);
+        return null;
+    }
 
-                voice.frequency.value = Calculations.shiftToOctave(notes, this.state.osc2.octave);
-                try {
-                    voice.start();
-                }catch (e) {
-                    console.log(e);
-                }
+    playStep() {
+        try {
+            let channel1 = this.getVoicesFor(1);
+            let channel2 = this.getVoicesFor(2);
+            let channel3 = this.getVoicesFor(3);
+            let channel4 = this.getVoicesFor(4);
+
+            let newState = this.getStateCopy();
+
+            if(channel1) {
+                channel1[0].start();
+                channel1[1].start();
+
+                newState.voices[0][1] = channel1[0];
+                newState.voices[1][1] = channel1[1];
             }
+            if(channel2) {
+                channel2[0].start();
+                channel2[1].start();
+
+                newState.voices[0][2] = channel2[0];
+                newState.voices[1][2] = channel2[1];
+            }
+            if(channel3) {
+                channel3[0].start();
+                channel3[1].start();
+
+                newState.voices[0][3] = channel3[0];
+                newState.voices[1][3] = channel3[1];
+            }
+            if(channel4) {
+                channel4[0].start();
+                channel4[1].start();
+
+                newState.voices[0][4] = channel4[0];
+                newState.voices[1][4] = channel4[1];
+            }
+
+            this.setState(newState);
+        }catch (e) {
+            console.log(e);
         }
     }
 
     resetAllVoices() {
-        this.state.voices.forEach((osc) => {
-            osc.forEach((voice) => {
+        this.state.voices.forEach((osc, o) => {
+            osc.forEach((voice, v) => {
                 if(voice) {
                     try {
-                        voice.stop();
+                        this.state.voices[o][v].stop();
+                        this.state.voices[o][v] = null;
                     } catch (e) {
                         //ignore
                     }
@@ -150,9 +173,31 @@ export default class Synth extends Component {
         let newState = this.getStateCopy();
         newState.osc1.mix = mix;
         this.setState(newState);
+
+        this.oscGain1.gain.value = mix;
     }
 
     handleOctave1Changed(octave) {
+        let newState = this.getStateCopy();
+        newState.osc1.octave = octave;
+        this.setState(newState);
+    }
+
+    handleWaveform2Changed(waveform) {
+        let newState = this.getStateCopy();
+        newState.osc2.waveform = waveform;
+        this.setState(newState);
+    }
+
+    handleMix2Changed(mix) {
+        let newState = this.getStateCopy();
+        newState.osc2.mix = mix;
+        this.setState(newState);
+
+        this.oscGain2.gain.value = mix;
+    }
+
+    handleOctave2Changed(octave) {
         let newState = this.getStateCopy();
         newState.osc1.octave = octave;
         this.setState(newState);
@@ -184,9 +229,9 @@ export default class Synth extends Component {
                         </div>
                         <div className="column is-2">
                             <Oscillator id="OSC-2"
-                                        onWaveformChanged={(waveform) => {this.handleWaveform1Changed(waveform)}}
-                                        onMixChanged={(mix) => {this.handleMix1Changed(mix)}}
-                                        onOctaveChanged={(octave) => {this.handleOctave1Changed(octave)}}
+                                        onWaveformChanged={(waveform) => {this.handleWaveform2Changed(waveform)}}
+                                        onMixChanged={(mix) => {this.handleMix2Changed(mix)}}
+                                        onOctaveChanged={(octave) => {this.handleOctave2Changed(octave)}}
                             />
                         </div>
                         <div className="column">
